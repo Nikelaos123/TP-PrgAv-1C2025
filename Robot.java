798573e477e00dcda913e43200baed256bf8f72c
Gianvl <gianfrancoliberati1@gmail.com>
2025-06-08 19:13:01 -0300

    Commit de clase Robot

---
+++ Robot.java
@@ -0,0 +1,12 @@
+public class Robot {
+    private int id;
+    private int carga;
+    private String estado;
+
+
+    public Robot(int id){
+        this.id = id;
+        this.carga = 100;
+        this.estado = "En espera";
+    }
+}
