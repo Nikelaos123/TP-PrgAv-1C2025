d3f060cc36b24506a6fa8bb4aa665ddc944efeb3
Gianvl <gianfrancoliberati1@gmail.com>
2025-06-08 19:21:24 -0300

    Commit de Red*

--- Robot.java
+++ Robot.java
@@ -2,11 +2,16 @@ public class Robot {
     private int id;
     private int carga;
     private String estado;
+    private Coordenada ubicacion; // CLASE PARA LABURAR EN DONDE ESTA
 
 
     public Robot(int id){
         this.id = id;
         this.carga = 100;
         this.estado = "En espera";
+    }
+
+    public void cargarBateria(){
+        this.carga = 100;
     }
 }
